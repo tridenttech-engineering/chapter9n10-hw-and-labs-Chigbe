776d7256db876b85cdab36f2d81d0d9e078750a1
Chigbe <Christianhigbe20@gmail.com>
2024-08-05 19:36:16 +0000

    Submission Ch9&10

--- hw10-13.cpp
+++ hw10-13.cpp
@@ -1,14 +1,12 @@
-//Lab 9-2.cpp - displays two monthly car payments
+//hw10-13.cpp - displays two monthly car payments
 //Created/revised by <your name> on <current date>
 
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 using namespace std;
-
 //function prototype
-double getPayment(int, double, int);
-
+void getPayment(int,double,int,double&);
 int main()
 {
     //declare variables
@@ -19,7 +17,8 @@ int main()
     int term = 0;
     double creditPayment = 0.0;
     double dealerPayment = 0.0;
-
+    double totalCreditPayment = 0.0;
+    double totalDealerPayment = 0.0;
     cout << "Car price (after any trade-in): ";
     cin >> carPrice;
     cout << "Rebate: ";
@@ -30,31 +29,28 @@ int main()
     cin >> dealerRate;
     cout << "Term in years: ";
     cin >> term;
-
     //call function to calculate payments
-    creditPayment = getPayment(carPrice - rebate,
-        creditRate / 12, term * 12);
-    dealerPayment = getPayment(carPrice, 
-        dealerRate / 12, term * 12);    //assign values to calculate payments
-    
+    getPayment(carPrice - rebate, creditRate / 12, term * 12, creditPayment);
+    getPayment(carPrice, dealerRate / 12, term * 12, dealerPayment);
+    totalCreditPayment = creditPayment * term * 12;
+    totalDealerPayment = dealerPayment * term * 12;
     //display payments
     cout << fixed << setprecision(2) << endl; 
-    cout << "Credit union payment: $" 
-        << creditPayment << endl;
-    cout << "Dealer payment: $"
-        << dealerPayment << endl;
-    
+    cout << "Credit union payment: $" << creditPayment << endl;
+    cout << "Total credit union payment: $" << totalCreditPayment << endl;
+    cout << "Dealer payment: $" << dealerPayment << endl;
+    cout << "Total dealer payment: $" << totalDealerPayment << endl;
     return 0;
 }//end of main function    
-
     //*****function definitions*****
-double getPayment(int prin,
-                  double monthRate, 
-                  int months)
+void getPayment(int prin,double monthRate,int months,double &payment)
 {
     //calculates and returns a monthly payment
     double monthPay = 0.0;
-    monthPay = prin * monthRate / 
-        (1 - pow(monthRate + 1, -months));
-    return monthPay;
+    if (monthRate == 0 && months == 0) {
+        payment = 0;
+        return;
+    }
+    monthPay = prin * monthRate / (1 - pow(monthRate + 1, -months));
+    payment = monthPay; 
 } //end of getPayment function//*****function definition*****
